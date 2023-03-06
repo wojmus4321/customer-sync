d90b9fa935ae6fc2d2c5d2c99e48e698753493cb
Wojciech Musiał <wojciech@musial.in>
2023-03-06 14:47:14 +0100

    refactor test, add AssertJ to increase readability, create custom CustomerAssert

---
+++ java/src/test/java/codingdojo/CustomerAssert.java
@@ -0,0 +1,44 @@
+package codingdojo;
+
+import org.assertj.core.api.AbstractAssert;
+import org.assertj.core.api.Assertions;
+
+public class CustomerAssert extends AbstractAssert<CustomerAssert, Customer> {
+    public CustomerAssert(Customer customer) {
+        super(customer, CustomerAssert.class);
+    }
+
+    public static CustomerAssert assertThat(Customer actual) {
+        return new CustomerAssert(actual);
+    }
+
+    public CustomerAssert isCompany() {
+        isNotNull();
+        if (actual.getCustomerType() != CustomerType.COMPANY) {
+            failWithMessage("Expected customer to be a company, but was %s", actual.getCustomerType());
+        }
+        return this;
+    }
+
+    public CustomerAssert hasEmptyMasterExternalId() {
+        isNotNull();
+        if (actual.getMasterExternalId() != null) {
+            failWithMessage("Expected master external id to be empty, but was %s", actual.getMasterExternalId());
+        }
+        return this;
+    }
+
+    public CustomerAssert wasUpdatedFrom(ExternalCustomer externalCustomer) {
+        isNotNull();
+
+        Assertions.assertThat(actual.getName()).isEqualTo(externalCustomer.getName());
+        Assertions.assertThat(actual.getExternalId()).isEqualTo(externalCustomer.getExternalId());
+        Assertions.assertThat(actual.getCompanyNumber()).isEqualTo(externalCustomer.getCompanyNumber());
+        Assertions.assertThat(actual.getAddress()).isEqualTo(externalCustomer.getPostalAddress());
+        Assertions.assertThat(actual.getShoppingLists()).isEqualTo(externalCustomer.getShoppingLists());
+        Assertions.assertThat(actual.getPreferredStore()).isEqualTo(externalCustomer.getPreferredStore());
+
+        return this;
+    }
+
+}
--- java/src/test/java/codingdojo/CustomerSyncTest.java
+++ java/src/test/java/codingdojo/CustomerSyncTest.java
@@ -1,48 +1,58 @@
 package codingdojo;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static codingdojo.CustomerAssert.assertThat;
 
 
 public class CustomerSyncTest {
 
+    private CustomerDataLayer db;
+    private CustomerSync sut;
+
+    @BeforeEach
+    void setUp() {
+        db = new InMemoryCustomerDataLayer();
+        sut = new CustomerSync(db);
+    }
+
     @Test
-    public void syncCompanyByExternalId(){
+    @DisplayName("Case1: " +
+            "given external company customer and existing company customer matching by externalId " +
+            "when sync is performed " +
+            "then existing company customer is updated with external company customer data correctly")
+    public void syncCompanyByExternalId() {
+        // given
+
         String externalId = "12345";
 
-        ExternalCustomer externalCustomer = createExternalCompany();
-        externalCustomer.setExternalId(externalId);
-
+        ExternalCustomer externalCustomer = createExternalCompany(externalId);
         Customer customer = createCustomerWithSameCompanyAs(externalCustomer);
-        customer.setExternalId(externalId);
 
-        CustomerDataLayer db = new InMemoryCustomerDataLayer();
         db.createCustomerRecord(customer);
-        CustomerSync sut = new CustomerSync(db);
 
-        // ACT
+        // when
         boolean created = sut.syncWithDataLayer(externalCustomer);
 
-        // ASSERT
-        assertFalse(created);
+        // then
+        Assertions.assertThat(created).isFalse();
         Customer updatedCustomer = db.findByExternalId(externalCustomer.getExternalId());
-        assertEquals(externalCustomer.getName(), updatedCustomer.getName());
-        assertEquals(externalCustomer.getExternalId(), updatedCustomer.getExternalId());
-        assertNull(updatedCustomer.getMasterExternalId());
-        assertEquals(externalCustomer.getCompanyNumber(), updatedCustomer.getCompanyNumber());
-        assertEquals(externalCustomer.getPostalAddress(), updatedCustomer.getAddress());
-        assertEquals(externalCustomer.getShoppingLists(), updatedCustomer.getShoppingLists());
-        assertEquals(CustomerType.COMPANY, updatedCustomer.getCustomerType());
-        assertNull(updatedCustomer.getPreferredStore());
+
+        assertThat(updatedCustomer)
+                .isCompany()
+                .hasEmptyMasterExternalId()
+                .wasUpdatedFrom(externalCustomer);
     }
 
 
-    private ExternalCustomer createExternalCompany() {
+    private ExternalCustomer createExternalCompany(String externalId) {
         ExternalCustomer externalCustomer = new ExternalCustomer();
-        externalCustomer.setExternalId("12345");
+        externalCustomer.setExternalId(externalId);
         externalCustomer.setName("Acme Inc.");
         externalCustomer.setAddress(new Address("123 main st", "Helsingborg", "SE-123 45"));
         externalCustomer.setCompanyNumber("470813-8895");
@@ -54,6 +64,7 @@ public class CustomerSyncTest {
         Customer customer = new Customer();
         customer.setCompanyNumber(externalCustomer.getCompanyNumber());
         customer.setCustomerType(CustomerType.COMPANY);
+        customer.setExternalId(externalCustomer.getExternalId());
         customer.setInternalId("45435");
         return customer;
     }
